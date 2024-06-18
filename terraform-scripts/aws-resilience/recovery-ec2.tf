418ee00bd8d2463bd1a34d3596e44c43c223bd69
OluwaTossin <temitopep450@gmail.com>
2024-06-18 12:16:24 +0100

    Rename resources in recovery scripts to avoid conflicts

--- terraform-scripts/aws-resilience/recovery-ec2.tf
+++ terraform-scripts/aws-resilience/recovery-ec2.tf
@@ -1,24 +1,14 @@
 provider "aws" {
-  alias  = "us-west-2-ec2"
-  region = "us-west-2" # Alternate region
+  alias  = "us-west-2"
+  region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  provider    = aws.us-west-2-ec2
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-}
-
-resource "aws_instance" "app" {
-  provider      = aws.us-west-2-ec2
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "recovery_app" {
+  provider      = aws.us-west-2
+  ami           = "ami-08a0d1e16fc3f61ea"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "AppInstance-Recovery"
+    Name = "RecoveryAppInstance"
   }
 }
