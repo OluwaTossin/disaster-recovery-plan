213687c936c8d04468cbab1e4048a00445525c2d
OluwaTossin <temitopep450@gmail.com>
2024-06-18 14:15:49 +0100

    Update AMI ID for us-west-2 region in recovery scripts

--- terraform-scripts/recovery/recovery-ec2.tf
+++ terraform-scripts/recovery/recovery-ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "recovery_app" {
   provider      = aws.us-west-2
-  ami           = "ami-0b20a6f09484773af"  # Updated AMI ID for us-west-2
+  ami           = "ami-0b20a6f09484773af"  # Updated
   instance_type = "t2.micro"
 
   tags = {
